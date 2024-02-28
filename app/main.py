33d6eda139c4e03b2e022bf340bdca2018640d9f
panalexeu <pan.alexeu@gmail.com>
2024-02-28 16:21:37 +0200

    attempts to add google auth (not successful)

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
+import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from mangum import Mangum
 from dotenv import load_dotenv
+from starlette.middleware.sessions import SessionMiddleware
 
-from .routers import health, todo, agent
+from .routers import health, todo, agent, auth
 
 
 @asynccontextmanager
@@ -14,11 +16,17 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(SessionMiddleware, secret_key=os.environ.get('SECRET_KEY'))
+
 app.include_router(health.router)
 app.include_router(todo.router)
 app.include_router(agent.router)
+app.include_router(auth.router)
 
 app_handler = Mangum(app)
 health_handler = Mangum(health.router)
 todo_handler = Mangum(todo.router)
 agent_handler = Mangum(agent.router)
+auth_handler = Mangum(agent.router)
+
